b4b279d980e40d446b1e7ca1ac61ded5fd2c084b
the-dsp-wizard <itamar.hakim@gmail.com>
2025-11-29 07:22:36 +0000

    Update app.py

--- infer/app.py
+++ infer/app.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import infer
+import wave
+import numpy as np
+import player
+import mido
 from io import BytesIO
 
-st.title("MIDI Humanizer")
-st.write("An RNN based MIDI file humanizer")
+st.title("Hello Streamlit!")
+st.write("This is a simple Streamlit app.")
 
 uploaded_file = st.file_uploader("Choose a file")
+
+amnt = st.slider("Select Humanization Amount", 0.0, 1.0, 0.5)
+release = st.slider("Select Synth Release Time", 0.1, 5.0, 0.5)
 
 # Check if a file was uploaded
 
@@ -21,16 +28,33 @@ if uploaded_file is not None:
     if content[:4] == b'MThd':
         st.success("This is a valid MIDI file.")
         midi_stream = BytesIO(content)
-        infer.process(midi_stream, "output.mid")
 
-        with open("output.mid", "rb") as f:
-            processed_content = f.read()
+        processed = infer.process(midi_stream, "output.mid", amnt)
 
         st.download_button(
             label="Download processed MIDI file",
-            data=processed_content,
+            data=processed,
             file_name="output.mid",
             mime="audio/midi"
         )
+
+        mid = mido.MidiFile(file=BytesIO(processed))
+
+        fs = 44100
+        synth = (player.synth(fs, release, mid) * 32767).astype(np.int16)
+
+        buffer = BytesIO()
+        with wave.open(buffer, "wb") as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(fs)
+            wf.writeframes(synth.tobytes())
+
+        # Reset buffer position
+        buffer.seek(0)
+
+        # Play in Streamlit
+        st.audio(buffer, format="audio/wav")
+
     else:
         st.error("This is not a MIDI file.")
